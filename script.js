470a91c0e7605575347a960d7f514746d891288a
PaulAlvarez13 <101508702+PaulAlvarez13@users.noreply.github.com>
2023-04-15 16:28:34 +0800

    added event listener

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var square = document.getElementById('square');
+
+function changeColor(color){
+    square.style.background = color;
+}
+
+square.addEventListener('click', (event) => changeColor('green'))
+
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
     let buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
